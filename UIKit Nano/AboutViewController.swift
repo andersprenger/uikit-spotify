ce7bfc5141e5352644b347e2552b383b5d368e2e
Anderson Sprenger <anderson.sprenger@edu.pucrs.br>
2021-06-22 22:05:48 -0300

    data flow ok, mas com problemas no resizing das labels

--- UIKit Nano/AboutViewController.swift
+++ UIKit Nano/AboutViewController.swift
@@ -12,21 +12,30 @@ class AboutViewController: UIViewController {
     var reciver:MusicCollection?
     var service:MusicService?
     
-    
     @IBOutlet weak var albumImage: UIImageView!
-    @IBOutlet weak var albumNameLabel: UILabel!
-    @IBOutlet weak var detailsLabel: UILabel!// data, songs number and artist
     
-    @IBOutlet weak var albumDescription: UILabel!
-    @IBOutlet weak var autorNameLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var detailsLabel: UILabel!
+    @IBOutlet weak var aboutAlbumLabel: UILabel!
     
-    @IBOutlet weak var autorDescriptionLabel: UILabel!
-    
+    @IBOutlet weak var aboutArtistTitleLabel: UIStackView!
+    @IBOutlet weak var aboutArtistLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         albumImage.image =  service?.getCoverImage(forItemIded: reciver!.id)
+        titleLabel.text = reciver?.title
+        var sum = TimeInterval()
+        for i in 0..<(reciver?.musics.count ?? 0) {
+            sum += (reciver?.musics[i].length) ?? 0
+        }
         
+        let formatter = DateComponentsFormatter()
+        formatter.allowedUnits = [.hour, .minute]
+        formatter.unitsStyle = .abbreviated
+        
+        detailsLabel.text = "Album by \(reciver?.mainPerson ?? "[Artist]") \n \(reciver?.musics.count) songs, \(formatter.string(from: sum))"
+        aboutAlbumLabel.text = reciver?.albumDescription ?? "not an album, treat me 🩹"
         
         
     }
