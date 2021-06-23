0ac6b3bf28215f5d15ece0f691808210491783e6
WillianAlbeche <willianalbeche@gmail.com>
2021-06-23 17:07:28 -0300

    queue view UI
    done

---
+++ UIKit Nano/QueueViewController.swift
@@ -0,0 +1,22 @@
+//
+//  QueueViewController.swift
+//  UIKit Nano
+//
+//  Created by Willian Magnum Albeche on 23/06/21.
+//
+
+import UIKit
+
+class QueueViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func buttonAction(_ sender: UIBarButtonItem) {
+        self.dismiss(animated: true)
+    }
+    
+}
