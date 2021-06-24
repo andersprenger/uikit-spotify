d240a335bd69e586c477b817568255b469ab536e
Anderson Sprenger <anderson.sprenger@edu.pucrs.br>
2021-06-24 02:01:59 -0300

    FavoritesViewController.swift - data flow "favorites-to-playing-segue" implementation

--- UIKit Nano/FavoritesViewController.swift
+++ UIKit Nano/FavoritesViewController.swift
@@ -37,9 +37,7 @@ class FavoritesViewController: UIViewController, UITableViewDelegate, UITableVie
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        service?.favoriteMusics
-        let cell =
-            favoriteTabView.dequeueReusableCell(withIdentifier: "favorite-cell", for: indexPath) as! FavoriteCell
+        let cell = favoriteTabView.dequeueReusableCell(withIdentifier: "favorite-cell", for: indexPath) as! FavoriteCell
         let cellItem: Music = (favorites?[indexPath.row])!
         cell.imageCell.image = service?.getCoverImage(forItemIded: cellItem.id)
         
@@ -49,8 +47,21 @@ class FavoritesViewController: UIViewController, UITableViewDelegate, UITableVie
         return cell
     }
     
+    // MARK: -- data flow (important for future reference)
     
     
+    // when a row is selected, it performs a segue to the player...
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "favorites-to-playing-segue", sender: indexPath)
+    }
     
-    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // geting the controller of the segue destination, which is between this and is our de facto destination
+        guard let navigationController = segue.destination as? UINavigationController else { return }
+        // then geting our de facto destination though navigationController.topViewController
+        guard let playerControler = navigationController.topViewController as? PlayingViewController else { return }
+        
+        // sending the data to our destination
+        playerControler.musicService = service
+    }
 }
