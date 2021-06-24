79c7887a711c6f2e9f44972746b74c6ac7a22de6
Anderson Sprenger <anderson.sprenger@edu.pucrs.br>
2021-06-24 17:23:32 -0300

    update favorites controller to don't use a duplicated struct

--- UIKit Nano/FavoritesViewController.swift
+++ UIKit Nano/FavoritesViewController.swift
@@ -9,11 +9,11 @@ import UIKit
 
 class FavoritesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    
     //id: 16a9b44f75da0f87aae8
     //id: 9673c3632d56ab7d6a7d
     
     private var service: MusicService?
-    private var favorites: [Music]?
     
     
     @IBOutlet weak var favoriteTabView: UITableView!
@@ -24,7 +24,6 @@ class FavoritesViewController: UIViewController, UITableViewDelegate, UITableVie
     override func viewDidLoad() {
         super.viewDidLoad()
         self.service = try? MusicService()
-        favorites = service?.favoriteMusics
         favoriteTabView.delegate  = self
         favoriteTabView.dataSource = self
         navigationItem.searchController = searchController
@@ -38,12 +37,12 @@ class FavoritesViewController: UIViewController, UITableViewDelegate, UITableVie
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = favoriteTabView.dequeueReusableCell(withIdentifier: "favorite-cell", for: indexPath) as! FavoriteCell
-        let cellItem: Music = (favorites?[indexPath.row])!
+        let cellItem: Music = (service?.favoriteMusics[indexPath.row])!
         cell.imageCell.image = service?.getCoverImage(forItemIded: cellItem.id)
         cell.service = service
-        cell.music = favorites![indexPath.row]
+        cell.music = service?.favoriteMusics[indexPath.row]
         
-        let isFavorite = service?.favoriteMusics.contains(favorites![indexPath.row])
+        let isFavorite = service?.favoriteMusics.contains((service?.favoriteMusics[indexPath.row])!)
         
         cell.favoriteButton.setImage(UIImage(systemName: isFavorite! ? "heart" : "heart.fill"), for: .normal)
   
@@ -60,7 +59,7 @@ class FavoritesViewController: UIViewController, UITableViewDelegate, UITableVie
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         performSegue(withIdentifier: "favorites-to-playing-segue", sender: indexPath)
         // ...and also set it's music as queue.nowPlaying
-        guard let selectedMusic = favorites?[indexPath.row] else { return }
+        guard let selectedMusic = service?.favoriteMusics[indexPath.row] else { return }
         service?.startPlaying(music: selectedMusic)
     }
     
@@ -80,7 +79,7 @@ class FavoritesViewController: UIViewController, UITableViewDelegate, UITableVie
     }
     
     func filterSearch(nameMusic: String) -> [Music]{
-        favorites?.filter({ Music in
+        service?.favoriteMusics.filter({ Music in
             Music.title.contains(nameMusic)
         }) ?? []
     }
